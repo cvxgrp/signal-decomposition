9e119bdf5fa57f3cc1d22ff2182fcca277ad8aee
Bennet Meyers <bennetm@stanford.edu>
2019-12-12 11:41:55 -0800

    treat parameters as a dictionary rather than a list

--- osd/components/component.py
+++ osd/components/component.py
@@ -9,10 +9,10 @@ Author: Bennet Meyers
 from abc import ABC, abstractmethod
 
 class Component(ABC):
-    def __init__(self, *values):
+    def __init__(self, **kwargs):
         self.__parameters = self._get_params()
         self.__cost = self._get_cost()
-        self.set_parameters(*values)
+        self.set_parameters(**kwargs)
         return
 
     @property
@@ -48,10 +48,8 @@ class Component(ABC):
     def _get_params(self):
         return None
 
-    def set_parameters(self, *values, ix=None):
-        if self.__parameters is not None and ix is None:
-            for p, v in zip(self.__parameters, values):
-                p.value = v
-        elif self.__parameters is not None and ix is not None:
-            self.__parameters[ix].value = values[0]
+    def set_parameters(self, **kwargs):
+        if self.__parameters is not None:
+            for key, value in kwargs.items():
+                self.__parameters[key].value = value
         return
