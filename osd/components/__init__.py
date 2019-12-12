ef702d1ec2c71209b33cca114978044b68d37ae6
Bennet Meyers <bennetm@stanford.edu>
2019-12-11 21:44:49 -0800

    sparse second difference class

--- osd/components/__init__.py
+++ osd/components/__init__.py
@@ -2,3 +2,4 @@ from osd.components.gauss_noise import GaussNoise
 from osd.components.smooth_second import SmoothSecondDifference
 from osd.components.smooth_first import SmoothFirstDifference
 from osd.components.norm1_first import SparseFirstDiffConvex
+from osd.components.norm1_second import SparseSecondDiffConvex
---
+++ osd/components/norm1_second.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+''' Smooth Component (1)
+
+This module contains the class for the convex heuristic for a piecewise linear
+function. A piecewise constant function has a sparse second-order difference;
+many changes in slope are exactly zero and a small number of them can be large.
+
+A convex approximation of this problem is minimizing the L1-norm of the second-
+order difference:
+
+    minimize || D_2 x ||_1
+
+This is an extension of the concept of Total Variation filtering, applied to the
+differences of a discrete signal, rather than the values.
+
+Author: Bennet Meyers
+'''
+
+import cvxpy as cvx
+from functools import partial
+from osd.components.component import Component
+from osd.utilities import compose
+
+class SparseSecondDiffConvex(Component):
+
+    def __init__(self, gamma=1e1):
+        super().__init__(gamma)
+        return
+
+    @property
+    def is_convex(self):
+        return True
+
+    def _get_cost(self):
+        diff2 = partial(cvx.diff, k=2)
+        gamma = self.parameters[0]
+        multiplier = lambda x: gamma * x
+        cost = compose(multiplier, cvx.sum, cvx.abs, diff2)
+        return cost
+
+    def _get_params(self):
+        gamma = cvx.Parameter(nonneg=True)
+        return [gamma]
