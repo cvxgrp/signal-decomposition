8bacf678aa733713913d1819dda2ed3952ed07ba
Bennet Meyers <bennetm@stanford.edu>
2019-12-17 13:26:53 -0800

    adding progress bar

--- osd/utilities.py
+++ osd/utilities.py
@@ -8,6 +8,7 @@ Author: Bennet Meyers
 '''
 
 import functools
+import sys
 
 def compose(*functions):
     """
@@ -20,3 +21,22 @@ def compose(*functions):
     def compose2(f, g):
         return lambda x: f(g(x))
     return functools.reduce(compose2, functions, lambda x: x)
+
+
+def progress(count, total, status='', bar_length=60):
+    """
+    Python command line progress bar in less than 10 lines of code. · GitHub
+    https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+    :param count: the current count, int
+    :param total: to total count, int
+    :param status: a message to display
+    :return:
+    """
+    bar_len = bar_length
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
