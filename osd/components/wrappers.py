9eb509df22b2517592bdf0fedf86a47446236f8a
Bennet Meyers <bennetm@stanford.edu>
2021-10-28 11:37:42 -0700

    add cost functions to wrappers

--- osd/components/wrappers.py
+++ osd/components/wrappers.py
@@ -14,6 +14,13 @@ def make_columns_equal(component):
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
 
+        def _get_cost(self):
+            f = super()._get_cost()
+            def g(x):
+                p = x.shape[1]
+                return p * f(x)
+            return g
+
         def prox_op(self, v, weight, rho):
             v_bar = np.average(v, axis=1)
             x_scalar = super().prox_op(v_bar, weight, rho * v.shape[1])
@@ -25,6 +32,13 @@ def make_columns_independent(component):
     class NewClass(component):
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
+
+        def _get_cost(self):
+            f = super()._get_cost()
+            def g(x):
+                p = x.shape[1]
+                return np.sum([f(x[:, i]) for i in range(p)])
+            return g
 
         def prox_op(self, v, weight, rho):
             f = super().prox_op
