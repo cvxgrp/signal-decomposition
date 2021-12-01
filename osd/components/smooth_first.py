0d01330445a72ff1ae7d71790750cbec62b352fa
Bennet Meyers <bennetm@stanford.edu>
2021-12-01 11:28:18 -0800

    subclass quad-lin

--- osd/components/smooth_first.py
+++ osd/components/smooth_first.py
@@ -11,21 +11,22 @@ import scipy.sparse as sp
 import numpy as np
 import cvxpy as cvx
 from functools import partial
-from osd.components.component import Component
+from osd.components.quad_lin import QuadLin
 from osd.utilities import compose
 from osd.components.quadlin_utilities import (
     build_constraint_matrix,
     build_constraint_rhs
 )
 
-class SmoothFirstDifference(Component):
+class SmoothFirstDifference(QuadLin):
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._c = None
-        self._u = None
-        self._last_weight = None
-        self._last_rho = None
+        P = None
+        q = None
+        r = None
+        F = None
+        g = None
+        super().__init__(P, q=q, r=r, F=F, g=g, **kwargs)
         return
 
     @property
@@ -38,43 +39,18 @@ class SmoothFirstDifference(Component):
         return cost
 
     def prox_op(self, v, weight, rho):
-        c = self._c
-        u = self._u
-        # print(np.sum(np.isnan(v)))
-        cond1 = c is None
-        cond2 = self._last_weight != weight
-        cond3 = self._last_rho != rho
-        if cond1 or cond2 or cond3:
-            n = len(v)
-            m1 = sp.eye(m=n-1, n=n, k=0)
-            m2 = sp.eye(m=n-1, n=n, k=1)
+        n = len(v)
+        if self.P is None:
+            m1 = sp.eye(m=n - 1, n=n, k=0)
+            m2 = sp.eye(m=n - 1, n=n, k=1)
             D = m2 - m1
-            P = 2 * D.T.dot(D) * weight
-            M = P + rho * sp.identity(P.shape[0])
-            # Build constraints matrix
-            A = build_constraint_matrix(
+            self.P = 2 * D.T.dot(D)
+            self.F = build_constraint_matrix(
                 n, self.period, self.vavg, self.first_val
             )
-            if A is not None:
-                M = sp.bmat([
-                    [M, A.T],
-                    [A, None]
-                ])
-            M = M.tocsc()
-            c = sp.linalg.factorized(M)
-            u = build_constraint_rhs(
-                len(v), self.period, self.vavg, self.first_val
-            )
-            self._c = c
-            self._u = u
-            self._last_weight = weight
-            self._last_rho = rho
-        if u is not None:
-            rhs = np.r_[rho * v, u]
-            out = c(rhs)
-            out = out[:len(v)]
-        else:
-            rhs = rho * v
-            out = c(rhs)
-        # print('returning', out)
-        return out
+            if self.F is not None:
+                self.g = build_constraint_rhs(
+                    len(v), self.period, self.vavg, self.first_val
+                )
+        vout = super().prox_op(v, weight, rho)
+        return vout
