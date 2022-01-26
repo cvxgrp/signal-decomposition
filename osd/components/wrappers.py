d42534fd22af1950f91e4f2026ac980d56585b25
Bennet Meyers <bennetm@stanford.edu>
2022-01-26 09:41:15 -0800

    wrapper calculates prox_counts to correctly weight average values in downstream prox_ops

--- osd/components/wrappers.py
+++ osd/components/wrappers.py
@@ -26,12 +26,15 @@ def make_columns_equal(component):
             with warnings.catch_warnings():
                 warnings.simplefilter('ignore')
                 v_bar = np.nanmean(v, axis=1)
+            prox_counts = np.sum(~np.isnan(v), axis=1)
+
             if use_set is not None:
                 use_reduced = np.any(use_set, axis=1)
             else:
-                use_reduced = None
-            x_scalar = super().prox_op(v_bar, weight, rho * v.shape[1],
-                                       use_set=use_reduced)
+                use_reduced = ~np.isnan(v_bar)
+            x_scalar = super().prox_op(v_bar, weight, rho,
+                                       use_set=use_reduced,
+                                       prox_counts=prox_counts)
             x = np.tile(x_scalar, (v.shape[1], True)).T
             return x
     return NewClass
