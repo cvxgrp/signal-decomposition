f222364a4b2e757be716566c720f344fc329ce30
Meyers-Im <bennetm@stanford.edu>
2023-05-01 11:28:25 -0700

    work on the SumSquareReference class

--- gfosd/components/sums.py
+++ gfosd/components/sums.py
@@ -17,18 +17,38 @@ class SumSquareReference(GraphComponent):
     phi(x;v) = || x - v ||_2^2 = ||x||_2^2 -2v^Tx + ||v||_2^2
     """
 
-    def __init__(self, vec, *args, **kwargs):
+    def __init__(self, vec, mask=None, *args, **kwargs):
         self._vec = vec
+        self._mask = mask
         super().__init__(*args, **kwargs)
+        if self._mask is not None:
+            self._has_helpers = True
+
+    def _set_z_size(self):
+        if self._mask is None:
+            self._z_size = (self._T - self._diff) * self._p
+        else:
+            self._z_size = self._mask.shape[0]
 
     def _make_P(self, size):
         return self.weight * 2 * sp.eye(size)  # note the (1/2) in canonical form!
 
     def _make_q(self):
-        self._q = (-2) * self._vec
+        if self._mask is None:
+            qx = self.weight * (-2) * self._vec
+            self._q = np.r_[qx, np.zeros(self.z_size)]
+        else:
+            qz = (-2) * self.weight * self._mask @ self._vec
+            self._q = np.r_[np.zeros(self.x_size), qz]
 
     def _make_r(self):
-        self._r = np.sum(np.square(self._vec))
+        if self._mask is None:
+            self._r = self.weight * np.sum(np.square(self._vec))
+        else:
+            self._r = self.weight * np.sum(np.square(self._mask @ self._vec))
+
+    def _make_A(self):
+        self._A = self._mask
 
 
 class SumAbs(GraphComponent):
