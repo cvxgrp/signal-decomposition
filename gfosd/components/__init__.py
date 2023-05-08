819acafee2f66464070b57f79e890c4d77ae0282
Meyers-Im <bennetm@stanford.edu>
2023-05-08 10:57:30 -0700

    fourier basis class

--- gfosd/components/__init__.py
+++ gfosd/components/__init__.py
@@ -5,7 +5,7 @@ from gfosd.components.sums import (SumSquare,
                                    SumCard,
                                    SumSquareReference)
 from gfosd.components.inequality_constraints import Inequality
-from gfosd.components.basis_constraints import Basis, Periodic
+from gfosd.components.basis_constraints import Basis, Periodic, Fourier
 from gfosd.components.finite_set import FiniteSet, Boolean
 from gfosd.components.aggregate import Aggregate
 from gfosd.components.equality_constraints import (FirstValEqual,
--- gfosd/components/basis_constraints.py
+++ gfosd/components/basis_constraints.py
@@ -11,10 +11,10 @@ that is avaible in the menu of g functions.
 
 """
 
-
 import numpy as np
 import scipy.sparse as sp
 from gfosd.components.base_graph_class import GraphComponent
+
 
 class Basis(GraphComponent):
     def __init__(self, basis, penalty=None, *args, **kwargs):
@@ -44,8 +44,8 @@ class Basis(GraphComponent):
             g = []
         else:
             g = [{'g': self._penalty,
-                         'args': {'weight': self.weight},
-                         'range': (0, size)}]
+                  'args': {'weight': self.weight},
+                  'range': (0, size)}]
         return g
 
     def _make_P(self, size):
@@ -55,8 +55,63 @@ class Basis(GraphComponent):
             P = self.weight * sp.eye(size)
         return P
 
+
 class Periodic(Basis):
     def __init__(self, period, *args, **kwargs):
         self._period = period
         M = sp.eye(period)
         super().__init__(M, *args, **kwargs)
+
+
+class Fourier(Basis):
+    def __init__(self, periods, num_harmonics, num_harmonics_cross=None, *args, **kwargs):
+        self._periods = np.atleast_1d(periods)
+        self._num_harmonics = num_harmonics
+        self._num_harmonics_cross = num_harmonics_cross
+        max_per = max(self._periods)
+        M = make_fourier_basis_cross(max_per, self._periods, self._num_harmonics, self._num_harmonics_cross)
+        super().__init__(M, *args, **kwargs)
+
+    def _make_B(self):
+        self._basis = make_fourier_basis_cross(self._T, self._periods, self._num_harmonics, self._num_harmonics_cross)
+        self._B = self._basis * -1
+
+
+def make_fourier_basis(T, p, K):
+    """
+    T: length of signal
+    p: length of period
+    K: number of harmonics
+    """
+    ts = np.arange(T)
+    sines = [np.sin(2 * (kix + 1) * np.pi * ts / p) for kix in range(K)]
+    cosines = [np.cos(2 * (kix + 1) * np.pi * ts / p) for kix in range(K)]
+    B = np.r_[sines, cosines].T
+    B /= np.max(B, axis=0)
+    return B
+
+
+def make_fourier_basis_cross(T, ps, K, crossK=None):
+    """
+    T: length of signal
+    ps: list of period lengths
+    K: number of harmonics
+    crossK: number of harmonics to keep in cross terms
+    """
+    if crossK is None:
+        crossK = min(2 * K, 2 * 8)
+    ps = np.atleast_1d(ps)
+    num_ps = len(ps)
+    wfs = [make_fourier_basis(T, p, K) for p in ps]
+    if num_ps == 1:
+        return np.r_[np.ones(T)[np.newaxis, :] / np.sqrt(T), wfs[0].T].T
+    elif num_ps == 2:
+        wfs_cross = np.einsum('ij,ik->ijk', wfs[0][:, :crossK], wfs[1][:, :crossK])
+        wfs_cross = wfs_cross.reshape((T, -1))
+        wfs_cross /= np.max(wfs_cross, axis=0)
+        return np.r_[np.ones(T)[np.newaxis, :] / np.sqrt(T), wfs[0].T, wfs[1].T, wfs_cross.T].T
+    elif num_ps == 3:
+        wfs_cross = np.einsum('ij,ik,il->ijkl', wfs[0][:, :crossK], wfs[1][:, :crossK], wfs[2][:, :crossK])
+        wfs_cross = wfs_cross.reshape((T, -1))
+        wfs_cross /= np.max(wfs_cross, axis=0)
+        return np.r_[np.ones(T)[np.newaxis, :] / np.sqrt(T), wfs[0].T, wfs[1].T, wfs[2].T, wfs_cross.T].T
